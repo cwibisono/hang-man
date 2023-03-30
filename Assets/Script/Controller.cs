134eceedc6da01bf21fe7f27a47f4cb59a018fc7
ario-labster <89965599+cwibisono@users.noreply.github.com>
2023-03-30 08:45:33 +0700

    Update script

--- Assets/Script/Controller.cs
+++ Assets/Script/Controller.cs
@@ -1,32 +1,79 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using MiscUtil.Collections.Extensions;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
 
 public class Controller : MonoBehaviour
 {
     public Text timeField;
     public Text wordToFindField;
     private float time;
-    private string[] wordsLocal = { "ONCOM", "TEMPE", "TAHU", "AYAM", "BEBEK", "TERONG" };
+    private string[] wordsLocal = { "ONCOM", "TEMPE", "TAHU", "AYAM GORENG", "BEBEK", "TERONG" };
     private string chosenWord;
     private string hiddenWord;
 
         // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Amount of items inside of wordsLocal is " + wordsLocal.Length);
+            
+        // for (int i = 0; i < wordsLocal.Length; i++)
+        // {
+        //     Debug.Log(wordsLocal[i]); 
+        // }
+        
         chosenWord = wordsLocal[Random.Range(0, wordsLocal.Length)];
-        wordToFindField.text = chosenWord;
-        Debug.Log(wordToFindField.text);
+        
+        for (int i = 0; i < chosenWord.Length; i++)
+        {
+            char letter = chosenWord[i];
+            if (char.IsWhiteSpace(letter))
+            {
+                hiddenWord += "  ";
+            }
+            else
+            {
+                hiddenWord += "_ ";
+            }
+        }
+
+        wordToFindField.text = hiddenWord;
+        //Debug.Log(wordToFindField.text);
 
     }
-
+    
     // Update is called once per frame
     void Update() 
     {
         time += Time.deltaTime;
         timeField.text = time.ToString();
     }
+
+    private void OnGUI()
+    {
+        Event e = Event.current;
+        if (e.type == EventType.KeyDown && e.keyCode.ToString().Length == 1)
+        {
+            string pressedLetter = e.keyCode.ToString();
+            
+            Debug.Log("Keydown event was triggered " + pressedLetter);
+            
+            if (chosenWord.Contains(pressedLetter))
+            {
+                int i = chosenWord.IndexOf(pressedLetter);
+                while (i !=-1)
+                {
+                    
+                }
+            }
+            else
+            {
+                
+            }
+        }
+    }
+    
 }
